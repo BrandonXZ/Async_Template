df165b4d575e775f874f92c4a604fc47996f7703
bsm <bsm@duostech.com>
2023-09-22 16:33:21 -0400

    chore: added macro call for tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
+#![allow(dead_code)]
+#![allow(unused)]
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+#[tokio::main]
+pub async fn main() {
     println!("Hello, world!");
 }
